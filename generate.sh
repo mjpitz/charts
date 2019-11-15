90f3ebfc99942c00d0511226f9a080704616b65c
Mya Pitzeruse <j.mya.pitz@gmail.com>
2019-11-14 22:33:15 -0600

    added new site

--- generate.sh
+++ generate.sh
@@ -4,19 +4,23 @@ readonly base_url="https://mjpitz.github.io/charts"
 
 # start off with the current gh-pages
 rm -rf dist/
-git clone --depth 1 -b gh-pages https://github.com/mjpitz/charts.git dist
+git clone -q --depth 1 -b gh-pages https://github.com/mjpitz/charts.git dist
 
 # port in any new templates
 for chart in $(find . -iname Chart.yaml | xargs dirname | cut -c 3-); do
+    echo "Packaging ${chart}"
     destination=dist/$(dirname ${chart})
     mkdir -p ${destination}
     helm package ${chart} -d ${destination}
 done
 
-for repo in $(ls -1 dist/); do
+repos=(stable)
+
+for repo in $repos; do
+    echo "Indexing ${repo}"
     repo_url="${base_url}/${repo}"
 
     helm repo index dist/${repo} --url "${repo_url}"
 done
 
-cp README.md dist/README.md
+cp site/* dist/
